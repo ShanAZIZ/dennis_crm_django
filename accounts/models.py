30f09f5920d2fe995d7c8670d3e94108bb6e5ee0
Shan_AZIZ <shan.azizh@gmail.com>
2020-12-17 16:37:42 +0400

    Commit _Video part 5 Premiere migration, Creation d'un superuser, d'un user, et des models Customer, Product et Order. Ajout des models dans admin.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    mail = models.EmailField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('indoor', 'Indoor'),
+        ('outdoor', 'OutDoor'),
+    )
+    name = models.CharField(max_length=200)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('pending', 'Pending'),
+        ('Out of Delivery', 'Out of Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # customer =
+    # product =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
